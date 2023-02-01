e2fc1ce6db90ce301c9a32824705f342b8dd3176
OSAMATAMER <59906580+OSAMATAMER@users.noreply.github.com>
2023-02-01 21:42:54 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,37 @@
+# This example requires the 'message_content' intent.
+
+import discord
+from discord import app_commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@tree.command(name = "code_block", description = "Rewrite the message in code block") 
+async def first_command(interaction, value:str):
+    await interaction.response.send_message("```" + value + "```")
+
+@tree.context_menu(name = "code_block")
+async def test(interaction, value: discord.Message):
+    await value.delete()
+    await interaction.response.send_message("```" + value.content + "```")
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('/code '):
+        await message.delete()
+        new = message.content.split("/code ")
+        await message.channel.send("```" + new[1] + "```")
+
+client.run('MTA3MDM1ODQ2Nzc5MTAzNjQ3Nw.GIm2ls.yyV1ZzgNUIp1qzcvMfLjTyOwy6f9rSLqKXGNsw')
